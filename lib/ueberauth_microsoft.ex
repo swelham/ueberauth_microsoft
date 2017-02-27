a663ca96b800787de74999904f9a413bb012fb07
stuart.welham@gmail.com <stuart.welham@gmail.com>
2017-02-27 19:39:57 +0000

    initial project creation

---
+++ lib/ueberauth_microsoft.ex
@@ -0,0 +1,18 @@
+defmodule UeberauthMicrosoft do
+  @moduledoc """
+  Documentation for UeberauthMicrosoft.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> UeberauthMicrosoft.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/ueberauth_microsoft_test.exs
@@ -0,0 +1,8 @@
+defmodule UeberauthMicrosoftTest do
+  use ExUnit.Case
+  doctest UeberauthMicrosoft
+
+  test "the truth" do
+    assert 1 + 1 == 2
+  end
+end
