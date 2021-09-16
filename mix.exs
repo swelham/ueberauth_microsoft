23d56e6efcbfdb6a1156eb9b71eda99992dd9c9c
Rolf Håvard Blindheim <rolf.havard.blindheim@intility.no>
2021-09-16 10:53:10 +0200

    Fix: extra scopes is not merged with existing scopes
    - `extra_scopes` from options was put in parameters using `extra_scopes` as key,
    rather than appending to default scopes.
    - Bump patch version

--- mix.exs
+++ mix.exs
@@ -2,7 +2,7 @@ defmodule UeberauthMicrosoft.Mixfile do
   use Mix.Project
 
   @source_url "https://github.com/swelham/ueberauth_microsoft"
-  @version "0.13.0"
+  @version "0.13.1"
 
   def project do
     [
